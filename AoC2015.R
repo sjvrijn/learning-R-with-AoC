2271006da3c37161b22775c3562bcde37c130cae
Sander van Rijn <svr003@gmail.com>
2023-01-07 01:52:11 +0100

    Add day 2

--- AoC2015.R
+++ AoC2015.R
@@ -20,4 +20,26 @@ day1 <- function() {
     print(floor)
 }
 
+day2 <- function() {
+    boxes <- scan(file="input2.txt", what=character())
+    wrapping_paper <- 0
+    ribbon <- 0
+    for (box in boxes) {
+        sides <- as.vector(unlist(strsplit(box, "x")), 'integer')
+
+        face1 <- sides[1] * sides[2]
+        face2 <- sides[1] * sides[3]
+        face3 <- sides[2] * sides[3]
+        min_face = min(c(face1, face2, face3))
+        wrapping_paper <- wrapping_paper + 2*face1 + 2*face2 + 2*face3 + min_face
+
+        max_side = max(sides)
+        wrap <- 2*(sides[1]+sides[2]+sides[3]) - 2*max_side
+        bow <- sides[1]*sides[2]*sides[3]
+        ribbon <- ribbon + wrap + bow
+    }
+    print(c(wrapping_paper,ribbon))
+}
+
 day1()
+day2()
